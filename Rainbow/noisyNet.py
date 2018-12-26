30fdf4bc378247c2aed1f7e8984847c8fe99e435
wangxing <xzw0005@gmail.com>
2018-12-25 18:39:52 -0600

    NoisyNet

--- Rainbow/noisyNet.py
+++ Rainbow/noisyNet.py
@@ -40,27 +40,6 @@ class NoisyLinear(nn.Linear):
             self.epsilon_bias.normal_()
             bias = bias + self.sigma_bias * self.epsilon_bias.data 
         return F.linear(input=x, weight=self.weight+self.sigma_weight*self.epsilon_weight.data, bias=bias)
-    
-class DQN(nn.Module):
-    def __init__(self, input_shape, num_actions):
-        super(DQN, self).__init__()
-        
-        self.conv = nn.Sequential(
-                nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
-                nn.ReLU(),
-                nn.Conv2d(32, 64, kernel_size=4, stride=2),
-                nn.ReLU(),
-                nn.Conv2d(64, 64, kernel_size=3, stride=1), 
-                nn.ReLU()
-            )
-        o = self.conv(torch.zeros(1, *input_shape))
-        conv_out_size = int(np.prod(o.size()))
-        self.fc = nn.Sequential(
-                nn.Linear(conv_out_size, 512),
-                nn.ReLU(),
-                nn.Linear(512, num_actions)
-            )
-        
 
 class NoisyDQN(nn.Module):
     def __init__(self, input_shape, num_actions):
